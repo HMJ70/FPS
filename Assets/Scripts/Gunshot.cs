f508d5c7d35a535b08ee3d84c16834606ecedb09
HMJ70 <hans.melvern1233@gmail.com>
2025-09-21 00:00:03 +0700

    GunAnim

--- Assets/Scripts/Gunshot.cs
+++ Assets/Scripts/Gunshot.cs
@@ -1,15 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class Gunshot : MonoBehaviour
 {
 
     [SerializeField] AudioSource gunshot;
+    [SerializeField] GameObject M9;
+    [SerializeField] bool canshoot = true;
 
     void Update()
     {
         if(Input.GetMouseButton(0))
         {
-            gunshot.Play();
+            if(canshoot == true)
+            {
+                canshoot = false;
+                StartCoroutine(Shoot());
+            }
         }
+    }
+
+    IEnumerator Shoot()
+    {
+        gunshot.Play();
+        M9.GetComponent<Animator>().Play("M9shoot");
+        yield return new WaitForSeconds(0.5f);
+        M9.GetComponent<Animator>().Play("New State");
+        yield return new WaitForSeconds(0.1f);
+        canshoot = true;
     }
 }
